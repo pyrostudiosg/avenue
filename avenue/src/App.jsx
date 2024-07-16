5579b25d65979a1322904e4c798b76567bd07ccb
ruiswang <110379602+pyrostudiosg@users.noreply.github.com>
2024-07-16 21:55:25 +0800

    update

--- avenue/src/App.jsx
+++ avenue/src/App.jsx
@@ -1,17 +1,5 @@
 
 import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Space from './components/Space'
-import Newsletter from './components/Newsletter'
-import Cards from './components/Cards'
-import Footer from './components/Footer'
-import Modeling from './components/Modeling'
-import Modelling from './components/Modelling'
-import Visualisation from './components/Visualisation'
-import VirtualSpace from './components/VirtualSpace'
-import Compositing from './components/Compositing'
-import Animation from './components/Animation'
 import {motion} from "framer-motion"
 import { useEffect } from 'react';
 
@@ -28,17 +16,7 @@ export default function App() {
       transition={{ duration: 0.6 }}
       className=' bg-[#ffffff]'
       > 
-        <Navbar />
-        <Hero />
-        <Modelling />
-        <Visualisation />
-        <VirtualSpace />
-        <Compositing />
-        <Animation />
-        {/* <Space /> */}
-        <Newsletter />
-        {/* <Cards /> */}
-        <Footer />
+        <h1>haha</h1>
       </motion.div>
     )
   }
--- avenue/src/router.jsx
+++ avenue/src/router.jsx
@@ -1,44 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
 import ARComponent from './ARComponent'
-import About from "./routes/About"
-import Service from './routes/Services'
-import Work from './routes/Work'
-import News from './routes/News'
-import Contact from './routes/Contact'
-import Uniiione from './work/uniiione'
-import Takami from './work/takami'
-import LloydsPartOne from './work/lloyds_part_one'
-import LloydsPartTwo from './work/lloyds_part_two'
-import LauraSweetMonsterSeries from './work/laura_sweet_monster_series'
-import LauraCyberpunkSeries from './work/laura_cyberpunk_series'
-import LauraSpaceCapsuleSeries from './work/laura_space_capsule_series'
-import LofficielXLouisVuitton from './work/lofficiel_china_louis_vuitton'
-import LarmoireVirtualStore from './work/larmoire_virtual_store'
-import AvenueJuvederm from './work/avenue_juvederm'
-import Kappa from './work/kappa'
-import Zhangweili from './work/zhangweili'
-
 
 export const router = createBrowserRouter([
     {path: "/", element: <App/>},
-    {path: "/about", element: <About/>},
-    {path: "/service", element: <Service/>},
-    {path: "/work", element: <Work/>},
-    {path: "/news", element: <News/>},
-    {path: "/contact", element: <Contact/>},
-    {path: "/work/uniiione", element: <Uniiione/>},
-    {path: "/work/takami", element: <Takami/>},
-    {path: "/work/lloyds_part_one", element: <LloydsPartOne/>},
-    {path: "/work/lloyds_part_two", element: <LloydsPartTwo/>},
-    {path: "/work/laura_sweet_monster_series", element: <LauraSweetMonsterSeries/>},
-    {path: "/work/laura_cyberpunk_series", element: <LauraCyberpunkSeries/>},
-    {path: "/work/laura_space_capsule_series", element: <LauraSpaceCapsuleSeries/>},
-    {path: "/work/lofficiel_china_louis_vuitton", element: <LofficielXLouisVuitton/>},
-    {path: "/work/larmoire_virtual_store", element: <LarmoireVirtualStore/>},
-    {path: "/work/avenue_juvederm", element: <AvenueJuvederm/>},
-    {path: "/work/kappa", element: <Kappa/>},
-    {path: "/work/zhangweili", element: <Zhangweili/>},
     {path: "/ar", element: <ARComponent/>},
     
   ])
