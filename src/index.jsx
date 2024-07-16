2bb3d8a6323217339e265c8a798d8d50bccfceaa
ruiswang <110379602+pyrostudiosg@users.noreply.github.com>
2024-07-16 23:50:51 +0800

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import {router} from "./router"
+import AR from './AR'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router = {router} />
+    {/* <RouterProvider router = {router} /> */}
+    <AR/>
   </React.StrictMode>,
 )
